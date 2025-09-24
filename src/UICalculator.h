f5363e70f52c713fc694aa168ae45d9a11fc6bdc
SekaniaMendez <chobidanielmendez@hotmail.com>
2025-09-24 16:09:51 -0600

    Botones en .h, empezando .cpp

--- src/UICalculator.h
+++ src/UICalculator.h
@@ -4,29 +4,49 @@
  * calculator.
  */
 #pragma once
-#include <QtWidgets>
+#include <QWidget>
+
+// Foward declaration helps optimize the software.
+class QGridLayout;
+class QLineEdit;
+class QPushButton;
 
 class UICalculator : public QWidget {
+  // Qt macro to initialize any QWidget class.
   Q_OBJECT
 private:
-  // shows the numbers or symbols that were press.
-  QLineEdit *symbolShower;
   // Use to organice the symbols in the window.
   QGridLayout *symbolOrganizer;
+  // shows the numbers or symbols that were press.
+  QLineEdit *symbolShower;
+  // Clear buttom.
   QPushButton *btnClr;
+  // Erase buttom.
+  QPushButton *btnBck;
+  // Hexadecimal buttom.
   QPushButton *btnHex;
+  // Octal buttom.
   QPushButton *btnOct;
+  // Decimal buttom.
   QPushButton *btnDec;
+  // Random buttom.
   QPushButton *btnRan;
+  // Equal buttom.
   QPushButton *btnEql;
-  QPushButton *btnRes;
+  // Substract buttom.
+  QPushButton *btnSub;
+  // Add-Sum buttom.
   QPushButton *btnAdd;
+  // Multiply buttom.
   QPushButton *btnMul;
+  // Division buttom.
   QPushButton *btnDiv;
+  // 0 - 9 buttoms.
+  QPushButton *digitButtons[10];
 
 public:
   // Constructor
-  explicit UICalculator(/* args */) { QWidget *parent = nullptr; }
+  explicit UICalculator(QWidget *parent = nullptr);
 
   // Destructor
   ~UICalculator() = default;
